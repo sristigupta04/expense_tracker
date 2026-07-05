d10fa4a39b7474ba0b00daa0cb3182c272624ac5
Sristi Gupta <sristigupta788@gmail.com>
2026-07-06 01:47:05 +0530

    client feature

--- src/pages/Reports.jsx
+++ src/pages/Reports.jsx
@@ -1,1 +1,48 @@
-L
+import { useState } from "react"
+
+
+
+
+export default function Reports(){
+  const [category, setCategory] = useState("");
+const [amount, setAmount] = useState("");
+const [budgets, setBudgets] = useState([]);
+function addBudget() {
+  const newBudget = {
+    id: Date.now(),
+    category,
+    amount,
+  };
+
+  setBudgets([...budgets, newBudget]);
+}
+ const spent = 4000;
+const remaining = amount - spent;
+    return(
+        <>
+        <div className="pie">pie Charts  </div>
+        {/* add budget */}
+        
+        {/* //budget card */}
+<div className="budget-card">
+  <ul>
+    {budgets.map((item, idx) => (
+      <li key={idx}>
+        <p>{item.food}</p>
+        <p>{item.spent}</p>
+        <p>{item.remaining}</p>
+      </li>
+    ))}
+  </ul>
+</div>
+
+{/* progress bar */}
+<div className="progress-bar">
+   
+  <p>Budget : ₹{amount}</p>
+<p>Spent : ₹{spent}</p>
+<p>Remaining : ₹{remaining}</p>
+</div>
+        </>
+    )
+}
--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -1,1 +1,36 @@
-B
+
+
+
+
+
+
+
+
+
+
+export default  function Transaction(){
+    const [title, setTitle] = useState("");
+const [amount, setAmount] = useState("");
+const [category, setCategory] = useState("");
+const [date, setDate] = useState("");
+const [type, setType] = useState("");
+const [transactions, setTransactions] = useState([]);
+
+
+
+return (
+    
+    {transaction.map((item) => (
+  <div key={item.id}>
+    <h3>{item.title}</h3>
+    <p>₹ {item.amount}</p>
+    <p>{item.category}</p>
+    <p>{item.date}</p>
+    <p>{item.type}</p>
+
+    <button>Edit</button>
+    <button>Delete</button>
+  </div>
+))}
+)
+}
